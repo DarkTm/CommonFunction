2c95c9f5d5e6cf308e6de6df62b450f624b0116d
tom <tom@tomtekiMacBook-Pro.local>
2013-06-09 00:51:48 +0000

    Initial Commit

---
+++ CommonFunction/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CommonFunction
+//
+//  Created by tom on 13-6-9.
+//  Copyright (c) 2013年 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CommonFunction/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CommonFunction
+//
+//  Created by tom on 13-6-9.
+//  Copyright (c) 2013年 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
